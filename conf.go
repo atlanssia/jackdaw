e03996b07fa8c0a6ba583c26c6228d3cc0796a7a
Manfred Wong <hi@atlanssia.com>
2015-10-23 16:54:56 +0800

    Create conf.go

---
+++ conf.go
@@ -0,0 +1,23 @@
+package main
+import (
+    "os"
+    "encoding/json"
+    "fmt"
+)
+
+type App struct {
+    Zookeepers []string `json:"zookeepers"`
+}
+
+var appConf App
+
+func initConf() {
+    file, _ := os.Open("app.json")
+    decoder := json.NewDecoder(file)
+    appConf = App{}
+    err := decoder.Decode(&appConf)
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+    fmt.Println(appConf)
+}

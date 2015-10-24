93c712e15ea3f7435f1a64aec40783fb0fd2564a
Atlanssia <hi@atlanssia.com>
2015-10-25 01:22:54 +0800

    in

---
+++ controller.go
@@ -0,0 +1,79 @@
+package main
+import (
+    "github.com/araframework/ara"
+    kfk "github.com/Shopify/sarama"
+    "net/http"
+    "encoding/json"
+    "fmt"
+    "time"
+    "github.com/samuel/go-zookeeper/zk"
+)
+
+type Controller struct {
+    ara.Controller
+}
+
+func (c *Controller) ListTopics(w http.ResponseWriter, r *http.Request) {
+
+    path := "/brokers/topics"
+    topics, err := c.lsChildren(path)
+    if err != nil {
+        writeError(w, err)
+        return
+    }
+
+    resp := make(map[string]string, len(topics))
+    for _, topic := range topics {
+        resp[topic] = "/uri/here?abc=123"
+    }
+
+    encoder := json.NewEncoder(w)
+    err = encoder.Encode(resp)
+    //    b, err := json.Marshal(topics)
+    //    if err != nil {
+    //        fmt.Println("error:", err)
+    //    }
+    //    w.Write(b)
+
+    // from kafka
+    client, err := kfk.NewClient([]string{"127.0.0.1:9092"}, kfk.NewConfig())
+    if err != nil {
+        panic(err)
+    }
+    defer client.Close()
+    
+    client.Config().ClientID = "jackdaw"
+    latestOffset, err := client.GetOffset("tpk001", 0, kfk.OffsetNewest)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("$$$$$$: %d\n", latestOffset)
+
+    //    bts, stat, ch, err := zc.GetW(path)
+    //    if err != nil {
+    //        panic(err)
+    //    }
+    //    fmt.Printf("%s *** %+v\n", string(bts), stat)
+
+    //    e := <-ch
+    //    fmt.Printf("--- %+v\n", e)
+    //    if e.Type == zk.EventNodeDataChanged {
+    //        watchData(zc)
+    //    }
+}
+
+
+func (c *Controller)lsChildren(path string) (children []string, err error) {
+    zc, _, err := zk.Connect(appConf.Zookeepers, time.Second)
+    if err != nil {
+        return
+    }
+    defer zc.Close()
+
+    children, _, err = zc.Children(path)
+    if err != nil {
+        return
+    }
+    return
+}
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "github.com/samuel/go-zookeeper/zk"
-    kfk "github.com/Shopify/sarama"
-    "net/http"
     "github.com/araframework/ara"
-    "encoding/json"
+    "reflect"
 )
 
 func init() {
@@ -15,74 +10,15 @@ func init() {
 }
 
 func main() {
+    // 1. tell the framework what type my controller is
     router := ara.NewRouter()
 
-    router.Handle("/", http.FileServer(http.Dir("static")))
-    router.HandleFunc("/topics", listTopics)
+    router.SetControllerValue(reflect.ValueOf(&Controller{}))
+
+//    router.Handle("/", http.FileServer(http.Dir("static")))
+//    router.HandleFunc("/topics", listTopics)
 
     ara.Start(router)
-}
-
-func listTopics(w http.ResponseWriter, r *http.Request) {
-
-    path := "/brokers/topics"
-    topics, err := lsChildren(path)
-    if err != nil {
-        writeError(w, err)
-        return
-    }
-
-    resp := make(map[string]string, len(topics))
-    for _, topic := range topics {
-        resp[topic] = "/uri/here?abc=123"
-    }
-
-    encoder := json.NewEncoder(w)
-    err = encoder.Encode(resp)
-//    b, err := json.Marshal(topics)
-//    if err != nil {
-//        fmt.Println("error:", err)
-//    }
-//    w.Write(b)
-
-    // from kafka
-    client, err := kfk.NewClient([]string{"127.0.0.1:9092"}, kfk.NewConfig())
-    if err != nil {
-        panic(err)
-    }
-    client.Config().ClientID = "jackdaw"
-    latestOffset, err := client.GetOffset("tpk001", 0, kfk.OffsetNewest)
-    if err != nil {
-        panic(err)
-    }
-
-    fmt.Printf("$$$$$$: %d\n", latestOffset)
-
-//    bts, stat, ch, err := zc.GetW(path)
-//    if err != nil {
-//        panic(err)
-//    }
-//    fmt.Printf("%s *** %+v\n", string(bts), stat)
-
-//    e := <-ch
-//    fmt.Printf("--- %+v\n", e)
-//    if e.Type == zk.EventNodeDataChanged {
-//        watchData(zc)
-//    }
-}
-
-func lsChildren(path string) (children []string, err error) {
-    zc, _, err := zk.Connect(appConf.Zookeepers, time.Second)
-    if err != nil {
-        return
-    }
-    defer zc.Close()
-
-    children, _, err = zc.Children(path)
-    if err != nil {
-        return
-    }
-    return
 }
 
 //ls /brokers/ids
